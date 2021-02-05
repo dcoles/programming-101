fc6ab86bb1be4d2398addf2eb75458e9673eb97a
David Coles <coles.david@gmail.com>
2021-02-05 08:48:15 -0800

    Use Comm for communicating with Canvas
    This is supported in both IPython and Colab.

--- programming_101/canvas.py
+++ programming_101/canvas.py
@@ -1,82 +1,99 @@
-import json
 import uuid
 
+from ipykernel.comm import Comm
 from IPython.display import display, HTML
-from google.colab import output
 
 
 class Canvas:
     def __init__(self, width, height):
         self.id = uuid.uuid4()
+        self.width = width
+        self.height = height
 
-        display(HTML(f'''
-            <canvas id="canvas_{ self.id.hex }" width="{ "%d" % width }" height="{ "%d" % height }"></canvas>    
+        context = {'id': self.id.hex, 'width': width, 'height': height}
+        display(HTML('''
+            <canvas id="canvas_%(id)s" width="%(width)d" height="%(height)d"></canvas>
             <script>
-                let canvas_ctx2d_{ self.id.hex } = canvas_{ self.id.hex }.getContext("2d");
+                Jupyter.notebook.kernel.comm_manager.register_target("canvas_%(id)s", (comm, msg) => {
+                    const ctx = canvas_%(id)s.getContext("2d");
+
+                    comm.on_msg((msg) => {
+                        const eventName = msg.content.data[0];
+                        const eventData = msg.content.data[1];
+                        switch (eventName) {
+                            // Properties
+                            case 'lineWidth':
+                            case 'strokeStyle':
+                            case 'fillStyle':
+                                ctx[eventName] = eventData[0];
+                                break;
+
+                            // Methods
+                            case 'beginPath':
+                            case 'lineTo':
+                            case 'moveTo':
+                            case 'stroke':
+                            case 'fill':
+                                ctx[eventName].apply(ctx, eventData);
+                                break;
+
+                            default:
+                                console.log('Unknown event: ' + eventName);
+                        }
+                    });
+                });
             </script>
-        '''))
+        ''' % context))
 
-    def _eval_this(self, js):
-        return output.eval_js(f'canvas_{ self.id.hex }.{ js }')
+        self.comm = Comm(f'canvas_{self.id.hex}')
 
-    def _eval_context2d(self, js):
-        return output.eval_js(f'canvas_ctx2d_{ self.id.hex }.{ js }')
-
-    @property
-    def width(self):
-        return self._eval_this('width')
-
-    @property
-    def height(self):
-        return self._eval_this('height')
+    def emit(self, event_name, *args):
+        self.comm.send([event_name, *args])
 
     @property
     def line_width(self):
-        return self._eval_context2d('lineWidth')
+        return NotImplemeted
 
     @line_width.setter
     def line_width(self, width):
-        self._eval_context2d(f'lineWidth = { json.dumps(width) }')
+        self.emit('lineWidth', [width])
 
     @property
     def fill_style(self):
-        return self._eval_context2d('fillStyle')
+        return NotImplemeted
 
     @fill_style.setter
     def fill_style(self, style):
-        self._eval_context2d(f'fillStyle = { json.dumps(style) }')
+        self.emit('fillStyle', [style])
 
     @property
     def stroke_style(self):
-        return self._eval_context2d('strokeStyle')
+        return NotImplemeted
 
     @stroke_style.setter
     def stroke_style(self, style):
-        self._eval_context2d(f'strokeStyle = { json.dumps(style) }')
+        self.emit('strokeStyle', [style])
 
     def stroke_rect(self, x, y, width, height):
-        self._eval_context2d(f'strokeRect({ json.dumps(x) }, { json.dumps(y) }, { json.dumps(width) }, { json.dumps(height) })')
+        self.emit('strokeRect', [x, y, width, height])
 
     def fill_rect(self, x, y, width, height):
-        self._eval_context2d(f'fillRect({ json.dumps(x) }, { json.dumps(y) }, { json.dumps(width) }, { json.dumps(height) })')
+        self.emit('fillRect', [x, y, width, height])
 
     def move_to(self, x, y):
-        self._eval_context2d(f'moveTo({ json.dumps(x) }, { json.dumps(y) })')
+        self.emit('moveTo', [x, y])
 
     def line_to(self, x, y):
-        self._eval_context2d(f'lineTo({ json.dumps(x) }, { json.dumps(y) })')
+        self.emit('lineTo', [x, y])
 
     def begin_path(self):
-        self._eval_context2d('beginPath()')
+        self.emit('beginPath')
 
     def close_path(self):
-        self._eval_context2d('closePath()')
+        self.emit('closePath')
 
     def stroke(self):
-        self._eval_context2d('stroke()')
+        self.emit('stroke')
 
     def fill(self, fill_rule=None):
-        if fill_rule:
-            self._eval_context2d(f'fill({ json.dumps(fill_rule) })')
-        else:
-            self._eval_context2d('fill()')
+        self.emit('fill', [fill_rule] if fill_rule is not None else [])
